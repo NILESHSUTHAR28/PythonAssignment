78b022880ccbff4daa95333d3ca7fa2f517e5be5
NILESHSUTHAR28 <nilu46446@gmail.com>
2025-12-18 18:09:15 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,21 @@
+# Task1
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("The area of the triangle is: ", addition)
+print("The area of the triangle is: ", subtraction)
+print("The area of the triangle is: ", multiplication)
+print("The area of the triangle is: ", division)
+
+
+# Task 2
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, " + first_name + " " + last_name + "! Welcome to Python programming.")
